2edecb3f5195df2818fb6e8df2ff27a8dc349f3f
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2024-02-25 18:02:23 -0300

    creates syntax utilities (like checking if a given lambda expression syntax has some attribute and covering all the possibilities of naming for that attribute)

---
+++ Arch.Nipah.Optimizations.SourceGenerator/SyntaxUtils.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arch.Nipah.Optimizations.SourceGenerator;
+
+public static class SyntaxUtils
+{
+    public static bool HasAttribute(this LambdaExpressionSyntax lambda, string attributeName, string attributeNms)
+        => lambda.AttributeLists.Any(a => a.Attributes.Any(a =>
+        {
+            var name = a.Name.ToString();
+            return name == attributeName || name == $"{attributeName}Attribute"
+                || name == $"{attributeNms}.{attributeName}" || name == $"{attributeNms}.{attributeName}Attribute";
+        }));
+}

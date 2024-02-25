bc04c977b5fda26e5d0a39702292fe0ed1e45110
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2024-02-25 17:13:00 -0300

    utilities for issuing errors, warning and informations from arbitrary syntax nodes and sometimes symbols

---
+++ Arch.Nipah.Optimizations.SourceGenerator/Utils.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arch.Nipah.Optimizations.SourceGenerator;
+
+public static class Utils
+{
+    public static void Error(this SourceProductionContext ctx, SyntaxNode at, string message)
+    {
+        ctx.ReportDiagnostic(Diagnostic.Create(
+            new DiagnosticDescriptor(
+                id: "NIP001",
+                title: message,
+                messageFormat: message,
+                category: "Arch.Nipah.Optimizations",
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true
+            ),
+            location: at.GetLocation()));
+    }
+    public static void Warning(this SourceProductionContext ctx, SyntaxNode at, string message)
+    {
+        ctx.ReportDiagnostic(Diagnostic.Create(
+            new DiagnosticDescriptor(
+                id: "NIP001",
+                title: message,
+                messageFormat: message,
+                category: "Arch.Nipah.Optimizations",
+                defaultSeverity: DiagnosticSeverity.Warning,
+                isEnabledByDefault: true
+            ),
+            location: at.GetLocation()));
+    }
+    public static void Info(this SourceProductionContext ctx, SyntaxNode at, string message)
+    {
+        ctx.ReportDiagnostic(Diagnostic.Create(
+            new DiagnosticDescriptor(
+                id: "NIP001",
+                title: message,
+                messageFormat: message,
+                category: "Arch.Nipah.Optimizations",
+                defaultSeverity: DiagnosticSeverity.Info,
+                isEnabledByDefault: true
+            ),
+            location: at.GetLocation()));
+    }
+
+    public static void Error(this SourceProductionContext ctx, ISymbol at, string message)
+    {
+        ctx.ReportDiagnostic(Diagnostic.Create(
+            new DiagnosticDescriptor(
+                id: "NIP001",
+                title: message,
+                messageFormat: message,
+                category: "Arch.Nipah.Optimizations",
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true
+            ),
+            location: at.Locations[0]));
+    }
+    public static void Warning(this SourceProductionContext ctx, ISymbol at, string message)
+    {
+        ctx.ReportDiagnostic(Diagnostic.Create(
+            new DiagnosticDescriptor(
+                id: "NIP001",
+                title: message,
+                messageFormat: message,
+                category: "Arch.Nipah.Optimizations",
+                defaultSeverity: DiagnosticSeverity.Warning,
+                isEnabledByDefault: true
+            ),
+            location: at.Locations[0]));
+    }
+    public static void Info(this SourceProductionContext ctx, ISymbol at, string message)
+    {
+        ctx.ReportDiagnostic(Diagnostic.Create(
+            new DiagnosticDescriptor(
+                id: "NIP001",
+                title: message,
+                messageFormat: message,
+                category: "Arch.Nipah.Optimizations",
+                defaultSeverity: DiagnosticSeverity.Info,
+                isEnabledByDefault: true
+            ),
+            location: at.Locations[0]));
+    }
+}

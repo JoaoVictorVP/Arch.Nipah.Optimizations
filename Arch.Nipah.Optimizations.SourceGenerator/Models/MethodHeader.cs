897e59471143a37eae3098dd504f2f2f7f9dd7bc
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2024-03-17 19:54:18 -0300

    model for method header
    contains basic needed informations for the source generator to consume

---
+++ Arch.Nipah.Optimizations.SourceGenerator/Models/MethodHeader.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arch.Nipah.Optimizations.SourceGenerator.Models;
+
+public class MethodHeader
+{
+    public string MethodName { get; }
+    public string File { get; }
+    public BlockSyntax Scope { get; }
+    public SemanticModel Semantics { get; }
+    public HashSet<string> Usings { get; }
+
+    public MethodHeader(string methodName, string file, BlockSyntax scope, SemanticModel semantics, HashSet<string> usings)
+    {
+        MethodName = methodName;
+        File = file;
+        Scope = scope;
+        Semantics = semantics;
+        Usings = usings;
+    }
+}

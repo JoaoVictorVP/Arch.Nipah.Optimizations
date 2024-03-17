bd514b190c5fdd143d3025b75afb7888f3b9bc20
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2024-03-17 19:55:48 -0300

    query model
    holds informations related to individual queries inside a method that will be optimized

---
+++ Arch.Nipah.Optimizations.SourceGenerator/Models/QueryModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arch.Nipah.Optimizations.SourceGenerator.Models;
+
+public readonly struct QueryModel
+{
+    public MethodHeader Method { get; }
+    public int GlobalIndex { get; }
+    public string Namespace { get; }
+    public InvocationExpressionSyntax QueryCall { get; }
+
+    public QueryModel(MethodHeader method, int globalIndex, string @namespace, InvocationExpressionSyntax queryCall)
+    {
+        Method = method;
+        GlobalIndex = globalIndex;
+        Namespace = @namespace;
+        QueryCall = queryCall;
+    }
+}

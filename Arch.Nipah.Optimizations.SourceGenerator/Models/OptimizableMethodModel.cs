982727c4a8af87302995841202088d7fe66665cc
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2024-03-17 19:54:58 -0300

    optimizable method model
    holds the method header, as well as query models for hte optimization process

---
+++ Arch.Nipah.Optimizations.SourceGenerator/Models/OptimizableMethodModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arch.Nipah.Optimizations.SourceGenerator.Models;
+
+public class OptimizableMethodModel
+{
+    public MethodHeader Header { get; }
+    public List<QueryModel> Queries { get; } = new(32);
+
+    public OptimizableMethodModel(MethodHeader header)
+    {
+        Header = header;
+    }
+}

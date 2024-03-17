e2ab53d8bf2e1362a46092e71c94ed512140dd15
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2024-03-17 19:52:57 -0300

    splits a namespace into subnamespaces

---
+++ Arch.Nipah.Optimizations.SourceGenerator/CodeGenUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arch.Nipah.Optimizations.SourceGenerator;
+
+public static class CodeGenUtils
+{
+    public static IEnumerable<string> NamespaceAndSubNamespacesFrom(string nms)
+    {
+        var parts = nms.Split('.');
+        for (int i = 0; i < parts.Length; i++)
+            yield return string.Join(".", parts.Take(i + 1));
+    }
+}
